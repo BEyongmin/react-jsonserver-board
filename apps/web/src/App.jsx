b2f308553fc5d844fbc8b21eddf07a032bb22bd1
BEyongmin <ymjo@idevel.kr>
2026-09-14 18:25:42 +0900

    new: 초기 세팅 및 라우터 설정

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -1,25 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
 import AppHeader from './components/AppHeader.jsx'
 import PostDetail from './pages/PostDetail.jsx'
 import PostForm from './pages/PostForm.jsx'
 import PostList from './pages/PostList.jsx'
 
-// UI 검수용 하드코딩 값이다. list | detail | form 중 하나로 바꿔 각 화면을 확인한다.
-const PREVIEW_SCREEN = 'list'
-
-const screens = {
-  detail: PostDetail,
-  form: PostForm,
-  list: PostList,
-}
-
 export default function App() {
-  const Screen = screens[PREVIEW_SCREEN]
-
   return (
     <>
       <AppHeader />
       <main id="main" tabIndex="-1" className="shell page">
-        <Screen />
+        <Routes>
+          <Route path="/" element={<PostList />}></Route>
+          <Route path="/post/:id" element={<PostDetail />}></Route>
+          <Route path="/posts/:id/edit" element={<PostForm />}></Route>
+        </Routes>
       </main>
     </>
   )
